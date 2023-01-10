a036b33a71238633a2a9cc2250281e0c9ef13e2b
João Lenon <lenonSec7@gmail.com>
2023-01-09 23:49:37 -0300

    fix(exceptions): better logs in exception

--- src/Exceptions/NotImplementedConfigException.js
+++ src/Exceptions/NotImplementedConfigException.js
@@ -7,32 +7,30 @@
  * file that was distributed with this source code.
  */
 
-import { Config } from '@athenna/config'
-import { Exception } from '@athenna/common'
+import { Path, Exception } from '@athenna/common'
 
 export class NotImplementedConfigException extends Exception {
   /**
    * Creates a new instance of NotImplementedConfigException.
    *
    * @param {string} channelName
+   * @param {any[]} channels
    * @return {NotImplementedConfigException}
    */
-  constructor(channelName) {
-    const content = `Channel ${channelName} is not configured inside logging.channels object from config/logging file.`
+  constructor(channelName, channels) {
+    const content = `Channel ${channelName} is not configured inside logging.channels object from config/logging.${Path.ext()} file.`
 
     let help = ''
 
-    if (Config.get('logging.channels')) {
-      const availableConfigs = Object.keys(Config.get('logging.channels')).join(
-        ', ',
-      )
+    if (channels && channels.length) {
+      const availableConfigs = Object.keys(channels).join(', ')
 
       help += `Available configurations are: ${availableConfigs}.`
     } else {
-      help += `The "Config.get('logging.channels') is empty, maybe your configuration files are not loaded?`
+      help += `The "Config.get('logging.channels')" is empty, maybe your configuration files are not loaded?`
     }
 
-    help += ` Create your configuration inside channels object to use it. Or load your configuration files using "Config.safeLoad(Path.config('logging.js'))`
+    help += ` Create your configuration inside channels object to use it. Or load your configuration files using "Config.safeLoad(Path.config('logging.${Path.ext()}'))`
 
     super(content, 500, 'E_NOT_IMPLEMENTED_CONFIG_ERROR', help)
   }
