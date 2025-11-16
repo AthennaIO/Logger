83ac853ae133d13276bba75ed99543a9b7289dbd
jlenon7 <lenonSec7@gmail.com>
2025-11-16 13:27:20 -0300

    chore(npm): update dependencies

---
+++ src/standalone/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @athenna/logger
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export * from '#src/types'
+export * from '#src/logger/Logger'
+export * from '#src/drivers/Driver'
+export * from '#src/helpers/AwsLogger'
+export * from '#src/formatters/Formatter'
+export * from '#src/helpers/FactoryHelper'
+export * from '#src/factories/DriverFactory'
+export * from '#src/factories/FormatterFactory'
